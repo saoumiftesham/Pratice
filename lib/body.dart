09ca639d2aad4157bb7857806e0f4a18d664182a
User-PC <saoumiftesham115@gmail.com>
2025-09-08 00:01:24 +0600

    Initial Commit. Expended Container.

--- lib/body.dart
+++ lib/body.dart
@@ -5,26 +5,24 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Container(
-        height: 200,
-        width: 180,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          boxShadow:[
-            BoxShadow(
-              color: Colors.grey.shade500,
-              offset: Offset(4.0, 4.0),
-              blurRadius: 15.0,
-              spreadRadius: 1.0),
-            BoxShadow(
-              color: Colors.white,
-              offset: Offset(-4.0, -4.0),
-              blurRadius: 15.0,
-              spreadRadius: 1.0),
-          ],
+    return Column(
+      children: [
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Colors.deepPurple,
+          ),
         ),
-      ),
+        Container(
+          height: 200,
+          color: Colors.red,
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.blue,
+          ),
+        )
+      ],
     );
   }
 }
