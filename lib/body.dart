52d2d4e9e58285865efaeef0a79a438aac8ca288
User-PC <saoumiftesham115@gmail.com>
2025-10-06 02:08:45 +0600

    listview tutorial.get new dart file in this section.

--- lib/body.dart
+++ lib/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice_new/square.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -7,57 +8,16 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.limeAccent,
-              borderRadius: BorderRadius.circular(20)
-
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(20)
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.greenAccent,
-              borderRadius: BorderRadius.circular(20)
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 100,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(20)
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            height: 100,
-            decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(20)
-            ),
-          ),
-        )
+        MySquare(),
+        MySquare(),
+        MySquare(),
+        MySquare(),
+        MySquare(),
+        MySquare(),
+        MySquare(),
+        MySquare(),
+        MySquare(),
+        MySquare(),
       ],
     );
   }
